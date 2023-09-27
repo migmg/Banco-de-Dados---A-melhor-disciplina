2c959ce6986e19159b843dfe10ef34f1f0f1ce21
migmg <102702140+migmg@users.noreply.github.com>
2023-09-26 23:37:27 -0300

    Update istaExercicios02.sql

--- istaExercicios02.sql
+++ istaExercicios02.sql
@@ -3,15 +3,15 @@ BEGIN
 SELECT * FROM autor;
 END
 
-excerc 2CREATE PROCEDURE sp_LivrosPorCategoria(IN nome_categoria varchar(200))
+excerc 2 CREATE PROCEDURE sp_LivrosPorCategoria(IN nomes_das_categorias varchar(200))
 BEGIN
 select *
 from Livro
 inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
-where nome_categoria = Categoria.Nome;
+where nomes_das_categorias = Categoria.Nome;
 END;
 
-exerc 3 CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN nome_categoria varchar(200))
+exerc 3 CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN nomes_das_categorias varchar(200))
 BEGIN
 select count(Livro.Titulo) as numero_de_livros, Categoria.Nome as categoria
 from Livro
@@ -21,45 +21,45 @@ group by Categoria.Nome;
 END;
 
 
-exerc 4 CREATE PROCEDURE sp_VerificarLivrosCategoria(IN nome_categoria varchar(200), OUT resposta varchar(50))
+exerc 4 CREATE PROCEDURE sp_VerificarLivrosCategoria(IN nomes_das_categorias varchar(200), OUT res varchar(50))
 BEGIN
 DECLARE quantidade INT DEFAULT 0;
 	select count(Livro.Titulo) as number_de_livros
-    INTO quant
+    INTO quantidade
 	from Livro
 	inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
 	where nome_categoria = Categoria.Nome
 	group by Categoria.Nome
 	;
-IF quant > 0 THEN SET resposta = 'tem livros';
+IF quant > 0 THEN SET res = 'tem livros';
 ELSE 
-SET respost = 'não tem livros';
+SET res = 'não tem livros';
 END IF;
-select respost;
+select res;
 END;
 
-exerc 5 CREATE PROCEDURE sp_LivrosAteAno(IN ano year)
+exerc 5 CREATE PROCEDURE sp_LivrosAteAno(IN anos INT)
 BEGIN
 SELECT Titulo, Ano_Publicacao from Livro
 where Ano_Publicacao <= ano
 ORDER BY Ano_Publicacao desc;
 END;
 
-exerc 6 CREATE PROCEDURE sp_TitulosPorCategoria(IN nome_categoria varchar(400))
+exerc 6 CREATE PROCEDURE sp_TitulosPorCategoria(IN nomes_das_categorias varchar(400))
 BEGIN
 select Livro.Titulo, Categoria.Nome as categoria
 from Livro
 inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
-where nome_categoria = Categoria.Nome;
+where nomes_das_categorias = Categoria.Nome;
 END;
 
 exerc 7 CREATE PROCEDURE sp_AdicionarLivro(
-    in livro_titulo varchar(250),
+    in titulo_dos_livro varchar(250),
     in editora_id int,
-    in ano_publicacao int,
-    in numero_paginas int,
+    in ano_de_publicaca int,
+    in numero_das_paginas int,
     in categoria_id int,
-    out resposta varchar(150)
+    out re varchar(150)
 )
 begin
     declare livro_existe int;
@@ -70,11 +70,11 @@ begin
         set resposta = "Esse título exite";
     else
         insert into Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
-        values (livro_titulo, editora_id, ano_publicacao, numero_paginas, categoria_id);
+        values (titulo_dos_livro, editora_id,ano_de_publicaca, numero_das_paginas, categoria_id);
 
-        set resposta = "O livro foi adicionado";
+        set res = "O livro foi adicionado";
     end if;
-    select resposta;
+    select res;
 end;
 
 exerc 8 CREATE PROCEDURE sp_AutorMaisAntigo()
@@ -85,17 +85,11 @@ LIMIT 1;
 END
 
 
-exerc 9 CREATE PROCEDURE sp_LivrosAteAno(IN ano YEAR): Esta linha define o início da criação da stored procedure chamada sp_LivrosAteAno. A procedure é projetada para listar os livros publicados até um ano específico, e recebe um parâmetro chamado ano do tipo YEAR.
+exerc 9 CREATE PROCEDURE sp_ListarAutores(): Esta linha define o início da criação da stored procedure chamada sp_ListarAutores. A procedure é projetada para listar todos os autores, e não recebe nenhum parâmetro.
 
 BEGIN: O bloco BEGIN marca o início do corpo da stored procedure. A partir deste ponto, começamos a definir a lógica da procedure.
 
-SELECT Titulo, Ano_Publicacao: Esta é a consulta SQL que seleciona os títulos e anos de publicação dos livros.
-
-FROM Livro: Indica a tabela da qual estamos selecionando os dados, que é a tabela Livro.
-
-WHERE Ano_Publicacao <= ano: Esta parte da consulta filtra os resultados para encontrar os livros onde o ano de publicação é menor ou igual ao valor fornecido no parâmetro ano.
-
-ORDER BY Ano_Publicacao DESC;: Isso ordena os resultados por ano de publicação em ordem decrescente, o que significa que os livros mais recentes aparecerão no topo da lista.
+SELECT * FROM autor;: Esta é a consulta SQL que seleciona todos os registros da tabela autor. O asterisco (*) indica que estamos selecionando todas as colunas da tabela.
 
 END;: Isso marca o fim do corpo da stored procedure.
 
