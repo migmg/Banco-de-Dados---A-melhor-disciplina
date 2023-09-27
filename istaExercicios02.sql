da3f46311ac56e4e59ab5563c5c5e66b84409c9a
migmg <102702140+migmg@users.noreply.github.com>
2023-09-26 23:08:35 -0300

    Update istaExercicios02.sql

--- istaExercicios02.sql
+++ istaExercicios02.sql
@@ -19,3 +19,21 @@ inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
 where nome_categoria = Categoria.Nome
 group by Categoria.Nome;
 END;
+
+ex 4 
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN nome_categoria varchar(200), OUT resposta varchar(50))
+BEGIN
+DECLARE quantidade INT DEFAULT 0;
+	select count(Livro.Titulo) as number_de_livros
+    INTO quant
+	from Livro
+	inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
+	where nome_categoria = Categoria.Nome
+	group by Categoria.Nome
+	;
+IF quant > 0 THEN SET resposta = 'tem livros';
+ELSE 
+SET respost = 'não tem livros';
+END IF;
+select respost;
+END;
