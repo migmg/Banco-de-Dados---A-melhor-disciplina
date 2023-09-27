bf45e04495d16888b3973303335b97e8cf5f26fc
migmg <102702140+migmg@users.noreply.github.com>
2023-09-26 23:05:36 -0300

    Update istaExercicios02.sql

--- istaExercicios02.sql
+++ istaExercicios02.sql
@@ -3,4 +3,11 @@ BEGIN
 SELECT * FROM autor;
 END
 
+excerc 2CREATE PROCEDURE sp_LivrosPorCategoria(IN nome_categoria varchar(200))
+BEGIN
+select *
+from Livro
+inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
+where nome_categoria = Categoria.Nome;
+END;
 
