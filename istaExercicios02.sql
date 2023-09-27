4bf02047df2d6a66e7815b152d7d067a5bd526b9
migmg <102702140+migmg@users.noreply.github.com>
2023-09-26 23:10:50 -0300

    Update istaExercicios02.sql

--- istaExercicios02.sql
+++ istaExercicios02.sql
@@ -44,3 +44,11 @@ SELECT Titulo, Ano_Publicacao from Livro
 where Ano_Publicacao <= ano
 ORDER BY Ano_Publicacao desc;
 END;
+
+exerc 6 CREATE PROCEDURE sp_TitulosPorCategoria(IN nome_categoria varchar(400))
+BEGIN
+select Livro.Titulo, Categoria.Nome as categoria
+from Livro
+inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
+where nome_categoria = Categoria.Nome;
+END;
