2d9a31b01db3278cdb1c604853b527105283e006
migmg <102702140+migmg@users.noreply.github.com>
2023-10-25 18:19:54 -0300

    Update exerciciosDeFixacao20231025.sql

--- exercíciosdeixaçãoo4Bimestre/exerciciosDeFixacao20231025.sql
+++ exercíciosdeixaçãoo4Bimestre/exerciciosDeFixacao20231025.sql
@@ -3,3 +3,8 @@ after insert on Clientes
 for each row
 insert into Auditoria(mensagem, data_hora)
 values ('qualquer coisa', NOW()); 
+2)create trigger delete_clientes
+before delete on Clientes
+for each row
+insert into Auditoria(mensagem)
+values ('informando')
